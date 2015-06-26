e4e89ea7ec4e674c6982be3f848e23abe1730969
ultimatewebuk <ultimatewebuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-26 11:00:48 +0000

    added clearfix
    git-svn-id: https://plugins.svn.wordpress.org/vebra-properties/trunk@1188083 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/vebra_shortcode.php
+++ includes/vebra_shortcode.php
@@ -414,7 +414,7 @@ function vp_map_pins($tproperties) {
         $pstr .= "'".$property->address_custom."',";
         $pstr .= "'".$property->latitude."',";
         $pstr .= "'".$property->longitude."',";
-        $pstr .= "'".str_replace("'","\'","<div class=\"vp_marker_content\"><a href=\"".vp_propertyurl($property->vebraid)."\">".vp_propertyimage($property->vebraid,0,"vp_pin_image")."<br /><div class='vp_pin_details'><em>".$property->address_display."</em><br />".$property->property_type."</em><br /><span>&pound;".number_format($property->price,0,"",",")." ".$property->price_postfix."</span></div><div class='vp_pin_link'>View details</div></a></div>")."'";
+        $pstr .= "'".str_replace("'","\'","<div class=\"vp_marker_content clearfix\"><a href=\"".vp_propertyurl($property->vebraid)."\">".vp_propertyimage($property->vebraid,0,"vp_pin_image")."<br /><div class='vp_pin_details'><em>".$property->address_display."</em><br />".$property->property_type."</em><br /><span>&pound;".number_format($property->price,0,"",",")." ".$property->price_postfix."</span></div><div class='vp_pin_link'>View details</div></a></div>")."'";
         $pstr .= "]\r\n";
     }
     $pstr .= "];\r\n";
